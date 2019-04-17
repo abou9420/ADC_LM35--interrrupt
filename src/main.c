f69fea7c0807eb5d5c4821c477cc6d90dc8939ce
abou <a.samadi1369@gmail.com>
2019-04-17 14:02:18 +0430

    RXT impletation

--- src/main.c
+++ src/main.c
@@ -27,24 +27,28 @@ int main()
 	char	temper[5];
 	float localTemp=0;
 	uint8_t temper1[20];	
+	uint8_t dataPrt[3];
 	
+	// Hardware Init
 	HW_Init();
 	
+	// Lcd Init
 	lcd_init();
+	
 	lcd_clear();
-	lcd_gotoxy(1,4);
-	lcd_putsf("Temperatuer");
+	// lcd_gotoxy(1,4);
+	// lcd_putsf("Temperatuer");
 	
 	while(1)
 	{
 		localTemp= HW_ADC_Read();
 		
-		lcd_gotoxy(2,7);
-		sprintf(temper ,"T=%0.1f",localTemp);
-		lcd_putsf(temper);
+		// lcd_gotoxy(2,7);
+		// sprintf(temper ,"T=%0.1f",localTemp);
+		// lcd_putsf(temper);
 		
-		uint8_t len = sprintf(temper1 ,"T=%0.1f\n",localTemp);
 		// send temp
+		uint8_t len = sprintf(temper1 ,"T=%0.1f\n",localTemp);
 		UART_Send(LPC_UART2,temper1,len,BLOCKING);
 		Delay_RIT_ms(1000);
 		
@@ -54,6 +58,45 @@ int main()
 		while(UART_CheckBusy(LPC_UART2));
 		UART_SendByte(LPC_UART2,localTemp);
 	  */
+		
+		// Reseive Data Byte mode
+		// if(((LPC_UART2->LSR)>>0)&0x01)
+		// {
+		// 	// Data is ready
+		// 	uint8_t data= UART_ReceiveByte(LPC_UART2);
+		// 	if(data=='a' || data== 'A' )
+		// 	{
+		// 		UART_Send(LPC_UART2,temper1,len,BLOCKING);
+		// 	}
+		// }
+		/*-----------------------*/
+			/* |Header|command|END|  */
+			/* | 0xAA |  1    |0xbb| show tempreture on LCD */
+      /* | 0xAA |  1    |0xbb| clear tempreture on LCD */			
+			/*-----------------------*/
+		// Reseive Data Array mode
+		
+    UART_Receive(LPC_UART2,dataPrt,3,BLOCKING);
+    
+		if( dataPrt[0]==0xAA && dataPrt[1]==1 && dataPrt[2]==0xbb)
+		{
+	    lcd_clear();
+			lcd_gotoxy(1,4);
+	    lcd_putsf("Temperatuer");
+			lcd_gotoxy(2,7);
+		  sprintf(temper ,"T=%0.1f",localTemp);
+		  lcd_putsf(temper);
+		}
+		else if(dataPrt[0]==0xAA && dataPrt[1]==2 && dataPrt[2]==0xbb)
+		{ 
+			lcd_clear();
+			lcd_gotoxy(1,3);
+      lcd_putsf("Project By:");
+			lcd_gotoxy(2,1);
+		  lcd_putsf("Aboubakr Samadi");
+			
+		}
+		
 	}
 	
 }
